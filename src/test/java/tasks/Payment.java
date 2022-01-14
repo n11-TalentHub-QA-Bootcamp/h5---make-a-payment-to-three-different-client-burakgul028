f4c26226ffce10a12c73601b9def5261943cabf8
Burak Gül <burakgul2010@gmail.com>
2022-01-14 23:26:39 +0300

    Second commit

--- src/test/java/tasks/Payment.java
+++ src/test/java/tasks/Payment.java
@@ -6,16 +6,26 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.ClickOnElement;
 import net.serenitybdd.screenplay.actions.SendKeys;
 import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.By;
 import ui.HomePageElements;
+import ui.PaymentPageElements;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class Payment implements Task {
-    private String username="";
-    private String password="";
+    private String name="";
+    private String phone="";
+    private String amount="";
+    private String client="";
 
-    public Payment() {
-
+    public Payment(String name,
+                   String phone,
+                   String amount,
+                   String client) {
+        this.name = name;
+        this.phone = phone;
+        this.amount = amount;
+        this.client = client;
 
     }
 
@@ -24,13 +34,25 @@ public class Payment implements Task {
     @Step("{0} deposite to the eribank")
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Click.on(HomePageElements.PAYMENT_BTN)
+                Click.on(HomePageElements.PAYMENT_BTN),
+                Click.on(PaymentPageElements.PHONE_FIELD),
+                //send phone variable
+                SendKeys.of(this.phone).into(PaymentPageElements.PHONE_FIELD),
+                Click.on(PaymentPageElements.NAME_FIELD),
+                //Send name and amount variable
+                SendKeys.of(this.name).into(PaymentPageElements.NAME_FIELD),
+                SendKeys.of(this.amount).into(PaymentPageElements.AMOUNT_SCROLL),
+                Click.on(PaymentPageElements.COUNTRY_BUTTON),
+                Click.on(By.xpath("//android.widget.TextView[@text='"+this.client+"']")),
+                Click.on(PaymentPageElements.SEND_PAYMENT_BUTTON),
+                Click.on(PaymentPageElements.CONFIRM_BUTTON)
+
 
         );
     }
 
-    public static Payment login(){
-        return instrumented(Payment.class);
+    public static Payment type(String phone, String name, String amount, String client){
+        return instrumented(Payment.class,phone,name,amount,client);
     }
 
 
--- src/test/java/ui/HomePageElements.java
+++ src/test/java/ui/HomePageElements.java
@@ -5,4 +5,6 @@ import org.openqa.selenium.By;
 
 public class HomePageElements {
     public static Target PAYMENT_BTN = Target.the("payment button").located(By.id("com.experitest.ExperiBank:id/makePaymentButton"));
+
+    public static Target LOGOUT_BTN = Target.the("logout button").located(By.id("logoutButton"));
 }
